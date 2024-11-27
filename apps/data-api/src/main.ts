7958845fb0d523298b9fda78e10d7fd3edc177bb
Josinee <ja.besseling@student.avans.nl>
2024-11-27 10:15:16 +0100

    api interface

--- apps/data-api/src/main.ts
+++ apps/data-api/src/main.ts
@@ -6,10 +6,18 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ApiResponseInterceptor } from '@avans-nx-songlibrary/backend/dto'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
+  const corsOptions: CorsOptions = {};
+  app.enableCors(corsOptions);
+
+  app.useGlobalInterceptors(new ApiResponseInterceptor());
+
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
--- libs/shared/api/src/index.ts
+++ libs/shared/api/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/models/song.interface';
+export * from './lib/models/api-response.interface';
