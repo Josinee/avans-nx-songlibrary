98f88939d19afefe93ddccd7a08d16247c830a39
Josinee <ja.besseling@student.avans.nl>
2024-12-12 13:34:34 +0100

    Update environment.production.ts

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -6,5 +6,5 @@ export const environment: IEnvironment = {
     ROOT_DOMAIN_URL: 'https://nxworkshop.azurewebsites.net',
     dataApiUrl: 'https://avans-nx-songlibrary.onrender.com/api',
 
-    MONGO_DB_CONNECTION_STRING: 'mongodb+srv:josine:Wachtwoord@cluster.n2zfb.mongodb.net/song-library'
+    MONGO_DB_CONNECTION_STRING: 'mongodb+srv://josine:Wachtwoord@cluster.n2zfb.mongodb.net/song-library'
 };
