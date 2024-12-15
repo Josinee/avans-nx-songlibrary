f87d2a1e416045829723c0e6acf345f860adb8b4
Josinee <ja.besseling@student.avans.nl>
2024-12-15 11:57:23 +0100

    changed song detail ui

--- libs/frontend/features/src/lib/playlist/playlist-list/playlist-list.component.ts
+++ libs/frontend/features/src/lib/playlist/playlist-list/playlist-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PlaylistService } from '../playlist.service';
-import { ICreatePlaylist, IPlaylist } from '@avans-nx-songlibrary/api';
+import { ICreatePlaylist, IPlaylist, IUser } from '@avans-nx-songlibrary/api';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Location } from '@angular/common'
+import { LoginService } from '../../login/login.service';
 
 
 
@@ -14,15 +15,27 @@ import { Location } from '@angular/common'
 })
 export class PlaylistListComponent implements OnInit, OnDestroy {
     playlists: IPlaylist[] | null = null;
+    privatePlaylists: IPlaylist[] | null = null;
+    user!: IUser;
 
     subscription: Subscription | undefined = undefined;
 
-    constructor(private playlistService: PlaylistService, private route: ActivatedRoute, private router: Router, private location: Location) {}
+    constructor(private playlistService: PlaylistService, private route: ActivatedRoute, private router: Router, private location: Location, private loginService: LoginService) {}
 
     ngOnInit(): void {
         this.subscription = this.playlistService.list().subscribe((results) => {
             console.log(`results: ${results}`);
             this.playlists = results;
+        });
+        this.loginService.currentUser.subscribe(user => {
+            if(user){
+                this.user = user;
+                console.log("init", this.user)
+            }
+        });
+        this.subscription = this.playlistService.getPlaylistFromCreator(this.user._id).subscribe((results) => {
+            console.log(`results: ${results}`);
+            this.privatePlaylists = results;
         });
     }
 
