ef4cb9372aac2213168f1636afb3788e912d28c2
Josinee <ja.besseling@student.avans.nl>
2024-12-15 20:13:12 +0100

    changed env

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -7,9 +7,9 @@ export const environment: IEnvironment = {
     dataApiUrl: 'https://avans-nx-songlibrary.onrender.com/api',
     rcmndApiUrl: 'https://rcmnd-api.onrender.com/api',
 
-    MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING,
-    NEO4J_DB_CONNECTION_STRING: process.env.NEO4J_DB_CONNECTION_STRING,
-    NEO4J_DB_DATABASE_NAME: process.env.NEO4J_DB_DATABASE_NAME,
-    NEO4J_DB_USERNAME: process.env.NEO4J_DB_USERNAME,
-    NEO4J_DB_PASSWORD: process.env.NEO4J_DB_PASSWORD
+    MONGO_DB_CONNECTION_STRING: 'dummy',
+    NEO4J_DB_CONNECTION_STRING: 'dummy',
+    NEO4J_DB_DATABASE_NAME: 'dummy',
+    NEO4J_DB_USERNAME: 'dummy',
+    NEO4J_DB_PASSWORD: 'dummy'
 }
