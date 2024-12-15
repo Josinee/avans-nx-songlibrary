868a9cacde231bc1b71da5d7b0f36f9e1490db88
Josinee <ja.besseling@student.avans.nl>
2024-12-15 23:28:34 +0100

    .

--- apps/rcmnd-api/src/app/app.module.ts
+++ apps/rcmnd-api/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Neo4jModule } from 'nest-neo4j'
 import { Neo4jBackendModule } from '../../../../libs/backend/neo4j/src/index'
+import { environment } from '@avans-nx-songlibrary/shared/util-env';
+import { env } from 'process';
 
 @Module({
   imports: [Neo4jModule.forRoot({
     scheme: 'neo4j',
-    database: 'recommendations',
-    host: 'localhost',
+    database: environment.NEO4J_DB_DATABASE_NAME,
+    host: environment.NEO4J_DB_CONNECTION_STRING,
     port: 7687,
-    username: 'neo4j',
-    password: 'Wachtwoord'
+    username: environment.NEO4J_DB_USERNAME,
+    password: environment.NEO4J_DB_PASSWORD,
   }), Neo4jBackendModule ],
   controllers: [AppController,],
   providers: [AppService],
--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,13 +3,13 @@ console.log("production");
 export const environment: IEnvironment = {
     production: true,
 
-    ROOT_DOMAIN_URL: 'https://nxworkshop.azurewebsites.net',
+    ROOT_DOMAIN_URL: 'https://songlibrary.onrender.com/',
     dataApiUrl: 'https://avans-nx-songlibrary.onrender.com/api',
-    rcmndApiUrl: 'somehting something',
+    rcmndApiUrl: 'https://rcmnd-api.onrender.com/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://josine:Wachtwoord@cluster.n2zfb.mongodb.net/song-library',
-    NEO4J_DB_CONNECTION_STRING: 'neo4j+s://1b1e492e.databases.neo4j.io',
-    NEO4J_DB_DATABASE_NAME: 'Instance01',
+    NEO4J_DB_CONNECTION_STRING: '1b1e492e.databases.neo4j.io',
+    NEO4J_DB_DATABASE_NAME: 'neo4j',
     NEO4J_DB_USERNAME: 'neo4j',
     NEO4J_DB_PASSWORD: 'bH7kKQvOOmsFoXlUzl1Z8HfUT1i-uM3wRww9zOVYEpM',
 };
