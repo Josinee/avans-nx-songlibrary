b2ecbebc261b72b5453af401e1b6a651a66d7cf0
Josinee <153730648+Josinee@users.noreply.github.com>
2024-12-17 09:50:20 +0100

    Update environment.production.ts

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -7,9 +7,9 @@ export const environment: IEnvironment = {
     dataApiUrl: 'https://avans-nx-songlibrary.onrender.com/api',
     rcmndApiUrl: 'https://rcmnd-api.onrender.com/api',
 
-    MONGO_DB_CONNECTION_STRING: 'mongodb+srv://josine:Wachtwoord@cluster.n2zfb.mongodb.net/song-library',
-    NEO4J_DB_CONNECTION_STRING: '1b1e492e.databases.neo4j.io',
+    MONGO_DB_CONNECTION_STRING: '...',
+    NEO4J_DB_CONNECTION_STRING: '...',
     NEO4J_DB_DATABASE_NAME: 'neo4j',
     NEO4J_DB_USERNAME: 'neo4j',
-    NEO4J_DB_PASSWORD: 'bH7kKQvOOmsFoXlUzl1Z8HfUT1i-uM3wRww9zOVYEpM',
+    NEO4J_DB_PASSWORD: '...',
 };
